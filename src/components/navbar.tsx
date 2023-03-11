51af350172f502c7ef55a01dd020ef8cbe112509
unknown <shjemhurov@gmail.com>
2023-03-11 17:22:40 +0500

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <h2>Navbar</h2>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
