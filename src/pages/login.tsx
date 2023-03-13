22435627403dd2a435b4a7a80e054069de926947
unknown <shjemhurov@gmail.com>
2023-03-13 21:19:11 +0500

    login page created

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-// import "../components/signup.css";
-// import signupImg from "../assets/undraw_join_re_w1lh.svg";
-import { Card, Button } from "antd";
+import { Card } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import Navbar from "../components/navbar";
 
 const onFinish = (values: any) => {
   console.log("Success:", values);
@@ -14,17 +14,54 @@ const onFinishFailed = (errorInfo: any) => {
 export default function Login() {
   return (
     <>
-      <div className="signup_page">
-        <div className="signup_left">{/* <img src={signupImg} /> */}</div>
-        <div className="signup_box">
-          <Card className="signup_login_card">
-            {/* <h1>Sign up</h1> */}
-            <div className="card_buttons">
-              <button className="signup_btn">Sign Up</button>
-              <button className="signup_btn">Sign In</button>
-            </div>
-          </Card>
-        </div>
+      <Navbar />
+      <div className="register_main">
+        <Card className="register_form">
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
       </div>
     </>
   );
