2444ec39778a3e4c35f6fb703e65366b0bf31c87
unknown <shjemhurov@gmail.com>
2023-03-20 18:31:04 +0500

    adding todo to the list

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -5,15 +5,26 @@ import { useNavigate } from "react-router-dom";
 import { FaSignOutAlt } from "react-icons/fa";
 import { uid } from "uid";
 import { v4 as uuidv4 } from "uuid";
-import { set, ref } from "firebase/database";
+import { set, ref, onValue } from "firebase/database";
 
 export default function Todo() {
   const navigate = useNavigate();
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      if (!user) {
+      if (user) {
+        onValue(ref(db, `/${auth.currentUser?.uid}`), (snapshot) => {
+          setTodos([]);
+          const data = snapshot.val();
+          if (data !== null) {
+            Object.values(data).map((todo) => {
+              setTodos((prev) => [...prev, todo]);
+            });
+          }
+        });
+      } else if (!user) {
         navigate("/");
       }
     });
@@ -29,10 +40,13 @@ export default function Todo() {
 
   const writeToDatabase = () => {
     const uidd = uuidv4();
-    set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
-      todo: todo,
-      uid: uidd,
-    });
+    if (auth.currentUser) {
+      set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+        todo: todo,
+        uid: uidd,
+      });
+      setTodo("");
+    }
     setTodo("");
   };
 
@@ -53,7 +67,11 @@ export default function Todo() {
           <button onClick={writeToDatabase}>Add</button>
         </div>
 
-        <div></div>
+        <div className="ttodo">
+          {todos.map((todo, index) => (
+            <h3 key={index}>{todo.todo}</h3>
+          ))}
+        </div>
       </div>
     </>
   );
